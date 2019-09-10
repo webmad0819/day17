fc5ff0ddf8f8ae3ed99f9c84c41a6ca6a76e4ba1
dani <danivicario@dvicario.es>
2019-09-10 12:30:40 +0200

    updated server.js so I can have the years in the resulting JSON file as a number rather than string

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ fs.readFile("movies.json", "utf8", function (err, movies) {
 
     fs.writeFile("moviesClean.json", JSON.stringify(movies.map(movie => {
         movie.rate = +movie.rate;
+        movie.year = +movie.year;
         return movie
     })), function (err, data) {
         console.log("File was written successfully! :) namasté")
